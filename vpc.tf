f54313e57fba2092e50aeab9aab7ef6d8ed0b2e2
Omo Elias <Obedelias01@gmail.com>
2025-02-05 07:57:32 +0100

    Modified vpc file

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,6 @@
+# Create VPC
+# Create a VPC with the specified CIDR block. The VPC will have DNS support and DNS hostnames enabled. The VPC will be tagged with the project name and environment.
+# terraform aws vpc
 resource "aws_vpc" "vpc" {
   cidr_block =  var.vpc_cidr_block
   instance_tenancy = "default"
@@ -5,7 +8,17 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "vpc-${var.project_name}-${var.environment}"
+    Name = "${var.project_name}-${var.environment}-vpc"
   }
 
+}
+
+# Create an Internet Gateway and attach it to the VPC
+# terraform aws internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-igw"
+  }
 }
